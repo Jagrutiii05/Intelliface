edd16300c1ad47eb63201e000036a4c0e3dafb0f
jagrutiii05 <202151067@iiitvadodara.ac.in>
2024-04-12 20:17:56 +0530

    App.py modified

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def attendance():
     return render_template('index.html', selected_date=selected_date, attendance_data=attendance_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
